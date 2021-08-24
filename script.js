2c5ec8bf50de383baa85432b5a0d9aa74fb9d1bf
Michael Shea <mshea424@yahoo.com>
2021-08-24 14:18:54 -0400

    init testimonials

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ document.querySelectorAll('.cta-button').forEach( button => {
 
 // close button listener
 document.querySelector('#modal-close-btn').addEventListener( "click", hideModal);
+
+// testimonial slider
+
+const testimonials = document.querySelectorAll('.testimonial');
+
+let countIndex = 0;
+
+function incrementCount() {
+    
+    if (countIndex < testimonials.length - 1) {
+        countIndex++
+    } else {
+        countIndex = 0
+    };
+    console.log(countIndex)
+    console.log(testimonials.length - 1)
+    
+    testimonials[countIndex].classList.remove('testimonial-hidden');
+    testimonials[countIndex].classList.add('testimonial-shown');
+    
+    if (countIndex > 0) {
+        testimonials[countIndex - 1].classList.remove('testimonial-shown');
+        testimonials[countIndex - 1].classList.add('testimonial-hidden');
+    } else {
+        testimonials[testimonials.length - 1].classList.remove('testimonial-shown');
+        testimonials[testimonials.length - 1].classList.add('testimonial-hidden');
+    }
+};
+
+setInterval(incrementCount, 5000);
